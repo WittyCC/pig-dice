fc14b069edb94b3e8d2968aa8d1e81eca03bfa11
Witty Chang <wcc1213@gmail.com>
2017-06-14 09:22:30 -0700

    Initial commit.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Pick a random number between 1 and 6 for our roll of the dice.
+var dice1 = Math.floor(Math.random()*6 + 1);
+var dice2 = Math.floor(Math.random()*6 + 1);
+var score;
+
+// If statement so that if the roll is 1, the score is 0
+if (dice1 === 1 || dice2 === 1) {
+  score = 0;
+} else {
+  score = dice1 + dice2;
+}
+
+console.log("You rolled a "+dice+" for a score of "+score);
