033d6d09c744ff6e3c8a93a324c3e40dd79abb24
Witty Chang <wcc1213@gmail.com>
2017-06-14 16:28:48 -0700

    Second commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,30 @@
-// Pick a random number between 1 and 6 for our roll of the dice.
-var dice1 = Math.floor(Math.random()*6 + 1);
-var dice2 = Math.floor(Math.random()*6 + 1);
-var score;
+$(document).ready(function() {
+  $("btn").click(function(event) {
+    diceRoll();
+  });
+});
 
-// If statement so that if the roll is 1, the score is 0
-if (dice1 === 1 || dice2 === 1) {
-  score = 0;
-} else {
-  score = dice1 + dice2;
+var diceRoll = function() {
+var faceValue();
+var result = '';
+var diceCount = $("#dice")
 }
 
-console.log("You rolled a "+dice+" for a score of "+score);
+
+// function rollDice() {
+//   var d1 =
+// }
+//
+// // Pick a random number between 1 and 6 for our roll of the dice.
+// var dice1 = Math.floor(Math.random()*6 + 1);
+// var dice2 = Math.floor(Math.random()*6 + 1);
+// var score;
+//
+// // If statement so that if the roll is 1, the score is 0
+// if (dice1 === 1 || dice2 === 1) {
+//   score = 0;
+// } else {
+//   score = dice1 + dice2;
+// }
+//
+// console.log("You rolled a "+dice+" for a score of "+score);
